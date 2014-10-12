d34da3c09d6705011eed6bfd5e3261b68688318a
adelinor <adelinor@gmail.com>
2014-10-12 21:35:18 +0100

    Add country information to the admin city form

--- sample/with-route-only/js/controllers.js
+++ sample/with-route-only/js/controllers.js
@@ -5,13 +5,22 @@ angular.module('sample.controllers',[])
 	
 }])
 .controller('AdminCityCtrl', ['$scope', 'adminService', function($scope, adminService) {
+	
+	//Load countries
+	adminService.allCountries()
+	.then(function(data) {
+		$scope.countries = data;
+	});
+	
+	
 	$scope.add = function() {
 		$scope.errorMessage = undefined;
 		$scope.successMessage = undefined;
-		adminService.findCityByName($scope.searchName)
+		adminService.findCityByName($scope.searchName, $scope.country.code)
 		.then(function(data) {
 			if (! data) {
-				$scope.errorMessage = "Could not find a city with name '" + $scope.searchName + "'";
+				$scope.errorMessage = "Could not find a city with name '" +
+					$scope.searchName + "' in " + $scope.country.name;
 			} else {
 				$scope.successMessage = "Added city '" + data.name + "'";
 			}
