035b962313fe74a0f2688cb29565e1a44e6e1a20
adelinor <adelinor@gmail.com>
2014-10-17 05:08:43 +0100

    share code and templates between samples

--- sample/with-route-only/js/app.js
+++ sample/with-route-only/js/app.js
@@ -11,8 +11,8 @@ angular.module('sample.routes', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
 
 	$routeProvider.when('/home', {templateUrl: 'home/home.html', controller: 'HomeCtrl'});
-	$routeProvider.when('/admin', {templateUrl: 'admin/admin.html', controller: 'AdminCityCtrl'});
-	$routeProvider.when('/about', {templateUrl: 'about/about.html', controller: 'AppCtrl'});
+	$routeProvider.when('/admin', {templateUrl: '../common/admin/admin.html', controller: 'AdminCityCtrl'});
+	$routeProvider.when('/about', {templateUrl: '../common/about/about.html', controller: 'AppCtrl'});
 	$routeProvider.otherwise({redirectTo: '/home'});
 
 }]);
