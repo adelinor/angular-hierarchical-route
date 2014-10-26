4c870c602301e4ff1791ec194d6f943a2150b62a
adelinor <adelinor@gmail.com>
2014-10-26 20:21:47 +0000

    Helper lib is now stable and validated by example

--- angular-hierarchical-route.js
+++ angular-hierarchical-route.js
@@ -1,5 +1,5 @@
 /**
- * angular-hierarchical-route v0.0.1
+ * angular-hierarchical-route v0.1-SNAPSHOT
  * https://github.com/adelinor/angular-hierarchical-route
  */
 (function(window, angular, undefined) {'use strict';
